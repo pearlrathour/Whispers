0e5aa658eef0c247ed0f698e017ef000753e2475
Pearl <pearl.rathour33@gmail.com>
2023-08-11 23:45:22 +0530

    Level 4- Salting & Hashing

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express= require('express');
 const bodyParser= require('body-parser');
 const ejs= require('ejs');
 const { con, Users } = require('./models/Users');
-const md5= require("md5");
+// const md5= require("md5");
+const bcrypt= require("bcrypt");
 const { Port } = require('./config');
+const saltRounds= 10;
 
 const app= express();
 app.set('view engine', 'ejs');
@@ -23,25 +25,34 @@ app.get("/login", function(req, res){
 });
 
 app.post("/register", function(req,res){
+    // const newUser= new Users({
+    //     email: req.body.username,
+    //     password: md5(req.body.password)
+    // })
+    // newUser.save();
+
+    const hash=bcrypt.hashSync(req.body.password, saltRounds);
     const newUser= new Users({
         email: req.body.username,
-        password: md5(req.body.password)
-    })
+        password: hash
+    });
     newUser.save();
-
+    
     res.render('secrets');
 });
 
 app.post("/login", function(req,res){
     const username= req.body.username;
-    const password= md5(req.body.password);
+    // const password= md5(req.body.password);
+    const password= req.body.password;
 
     Users.findOne({email: username})
         .then(function (founduser) {
             if(founduser){
-                if(founduser.password === password){
-                    res.render('secrets');
-                }
+                if(bcrypt.compareSync(password, founduser.password) === true)
+                    res.render("secrets");
+                else
+                    console.log("Wrong passwrod")
             }
         })
         .catch(function (error) {
@@ -49,8 +60,6 @@ app.post("/login", function(req,res){
             res.send(error);
             console.error("Error fetching tasks:", error);
         });
-
-    res.render('secrets');
 });
 
 app.listen(process.env.Port, function(){
